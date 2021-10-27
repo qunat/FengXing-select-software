cae609312f484ef7b568b0beb9fc35d741302176
loujiand <304817449@qq.com>
2021-10-28 00:39:34 +0800

    测量模块外移

--- module/FuctionModule.py
+++ module/FuctionModule.py
@@ -1,8 +1,13 @@
 #coding=utf-8
+from OCC.Core.AIS import AIS_LengthDimension
+from OCC.Core.BRep import BRep_Tool
+from OCC.Core.Geom import Geom_Circle
 from OCC.Core.TopAbs import TopAbs_FACE, TopAbs_EDGE, TopAbs_VERTEX
-from OCC.Core.TopoDS import TopoDS_Builder, TopoDS_Compound
+from OCC.Core.TopoDS import TopoDS_Builder, TopoDS_Compound, TopoDS_Vertex, TopoDS_Edge, TopoDS_Face
+from OCC.Core.gp import gp_Pnt
 from OCC.Extend.DataExchange import write_step_file, write_iges_file, write_stl_file
-from PyQt5.QtWidgets import QFileDialog
+from OCC.Extend.TopologyUtils import TopologyExplorer
+from PyQt5.QtWidgets import QFileDialog, QMessageBox
 import re
 
 
@@ -105,3 +110,107 @@ def Measure_diameter_fun(self):
         self.canva._display.SetSelectionMode(TopAbs_EDGE)  # 设置选择模式
         self.statusbar.showMessage("请选择圆弧或者圆弧面")
         # print("选择面")
+def Measure_funtion(self,shp,*args):
+    try:
+        if self.measure_signale == 1:
+            for shape in shp:  # this should be a TopoDS_Edge
+                # print(type(shape))
+                if shape.IsNull():
+                    continue
+                elif isinstance(shape, TopoDS_Vertex):
+                    point = BRep_Tool.Pnt(shape)
+                    self.measure_shape_list.append(point)
+                elif isinstance(shape, TopoDS_Edge):
+                    select = BRep_Tool.Curve(shape)[0]
+                    if not Geom_Circle.DownCast(select):
+                        pass
+                    else:
+                        possible_circle = Geom_Circle.DownCast(select)
+                        # 从Geom_Curve向下类型转换为Geom_Circle
+                        circle_ = possible_circle.Circ()
+                        # 得到gp_Circ类型的圆
+                        loc = circle_.Location()  # 圆心， gp_Pnt 类型
+                        self.measure_shape_list.append(loc)
+
+                else:
+                    self.measure_shape_list.append(shape)
+
+                if len(self.measure_shape_list) == 2:
+                    try:
+                        if isinstance(self.measure_shape_list[0], gp_Pnt):
+                            distance = self.measure_shape_list[0].Distance(self.measure_shape_list[1])
+                            measure_result = (distance)
+
+                        else:
+                            am = AIS_LengthDimension(self.measure_shape_list[0], self.measure_shape_list[1])
+                            # self.canva._display.Context.Display(am,True)
+                            measure_result = am.GetValue()
+
+                        measure_result = "测量结果:" + "{:2.2f}".format(measure_result) + "mm"
+                        reply = QMessageBox.information(self,  # 使用infomation信息框
+                                                        "测量结果",
+                                                        str(measure_result),
+                                                        QMessageBox.Yes)
+                    except:
+                        self.statusbar.showMessage("必须选择同样的元素")
+                    finally:
+                        self.measure_shape_list.clear()
+
+                elif len(self.measure_shape_list) > 2:
+                    pass
+                    self.measure_shape_list.clear()
+
+
+        elif self.measure_signale == 2:  # 测量直径
+            for shape in shp:  # this should be a TopoDS_Edge
+                if shape.IsNull():
+                    continue
+                else:
+                    self.measure_shape_list.append(shape)
+
+                if len(self.measure_shape_list) == 1:
+                    try:
+                        if isinstance(self.measure_shape_list, TopoDS_Edge):
+                            select = BRep_Tool.Curve(self.measure_shape_list[0])[0]
+                            if not Geom_Circle.DownCast(select):
+                                pass
+                            else:
+                                possible_circle = Geom_Circle.DownCast(select)
+                                # 从Geom_Curve向下类型转换为Geom_Circle
+                                circle_ = possible_circle.Circ()
+                                # 得到gp_Circ类型的圆
+                                loc = circle_.Location()  # 圆心， gp_Pnt 类型
+                                radius = circle_.Radius()  # 半径
+                                measure_result = radius * 2
+                            # am = AIS_RadiusDimension(self.measure_shape_list[0], self.measure_shape_list[1])
+                        elif isinstance(self.measure_shape_list[0], TopoDS_Face):
+                            for e in TopologyExplorer(self.measure_shape_list[0]).edges():
+                                select = BRep_Tool.Curve(e)[0]
+                                if not Geom_Circle.DownCast(select):
+                                    pass
+                                else:
+                                    possible_circle = Geom_Circle.DownCast(select)  # 从Geom_Curve向下类型转换为Geom_Circle
+                                    circle_ = possible_circle.Circ()  # 得到gp_Circ类型的圆
+                                    loc = circle_.Location()  # 圆心， gp_Pnt 类型
+                                    radius = circle_.Radius()  # 半径
+                                    # print(loc.X(), loc.Y(), loc.Z(), radius)
+                                    # ls_point = [loc.X(), loc.Y(), loc.Z(), radius]
+                                    measure_result = radius * 2
+
+                                    break
+
+                        measure_result = "测量结果:直径" + "{:2.2f}".format(measure_result) + "mm"
+                        reply = QMessageBox.information(self,  # 使用infomation信息框
+                                                        "测量结果",
+                                                        str(measure_result),
+                                                        QMessageBox.Yes)
+                    except:
+                        self.statusbar.showMessage("必须选择同样的元素")
+                    finally:
+                        self.measure_shape_list.clear()
+
+                elif len(self.measure_shape_list) > 1:
+                    pass
+                    self.measure_shape_list.clear()
+    except:
+        pass
